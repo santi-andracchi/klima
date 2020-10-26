0e580bcb82c002d4280e83d00369c03dda4f88d5
Santiago Andracchi <santiago.andracchi@gmail.com>
2020-10-26 18:40:55 -0300

    Add secret property file. Update gradle-wrapper

--- app/src/main/java/com/testapp/klima/datasource/forecast/OpenWeatherForecastDataSource.kt
+++ app/src/main/java/com/testapp/klima/datasource/forecast/OpenWeatherForecastDataSource.kt
@@ -1,5 +1,6 @@
 package com.testapp.klima.datasource.forecast
 
+import com.testapp.klima.BuildConfig
 import com.testapp.klima.api.OpenWeatherApi
 import com.testapp.klima.model.ForecastResponse
 import com.testapp.klima.model.Result
@@ -22,7 +23,7 @@ class OpenWeatherForecastDataSource : ForecastDataSource {
     override suspend fun getForecast(lat: Double, lon: Double): Result<ForecastResponse> {
         return try {
             // TODO: add api key from build config
-            Result.Success(api.getForecast(lat, lon, "metric", "hourly,minutely", ""))
+            Result.Success(api.getForecast(lat, lon, "metric", "hourly,minutely", BuildConfig.OPEN_WEATHER_MAP_API_KEY))
         } catch (throwable: Throwable) {
             Result.Error(throwable.toErrorResult())
         }
